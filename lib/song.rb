62eb5a320e7b857369625f2b7743f447e130ccee
Victor Plushko <vicplushko33@gmail.com>
2020-04-13 00:38:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ class Song
   
   def self.new_from_filename(filename)
     song = self.new
-    song.name = filename.split(" - ") [1].split(" . ") [0]
+    song.name = filename.split(" - ") [1].split(".") [0]
     song.artist_name = filename.split(" - ") [0]
     song
   end
