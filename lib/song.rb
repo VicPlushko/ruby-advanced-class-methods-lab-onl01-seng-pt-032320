7a677d005d54eca56977cb99321dfe8f41916230
Victor Plushko <vicplushko33@gmail.com>
2020-04-13 00:43:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,4 +46,11 @@ class Song
     song.artist_name = filename.split(" - ") [0]
     song
   end
+  
+  def self.create_from_filename(filename)
+    song = self.new
+    song.name = filename.split(" - ") [1].split(".") [0]
+    song.artist_name = filename.split(" - ") [0]
+    song.save
+  end
 end
